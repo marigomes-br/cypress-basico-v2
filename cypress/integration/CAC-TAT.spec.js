9602231413a55c19e43a3b6cf2b6fc710355d4ff
Mariana Gomes Santos <84389127+marigomes-br@users.noreply.github.com>
2024-06-08 18:43:08 -0300

    ended lesson 1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,29 +9,117 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
     });
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function() {
-        cy.get('input[id="firstName"]')
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        cy.get('#firstName')
         .should('be.visible')
-        .type('Mari')
+        .type('Mari', { delay: 10})
         .should('have.value', 'Mari');
 
-        cy.get('input[id="lastName"]')
+        cy.get('#lastName')
         .should('be.visible')
-        .type('Gomes')
+        .type('Gomes', { delay: 10})
         .should('have.value', 'Gomes');
 
-        cy.get('input[id="email"]')
+        cy.get('#email')
         .should('be.visible')
-        .type('maregomes@outlook.com')
+        .type('maregomes@outlook.com', { delay: 10})
         .should('have.value', 'maregomes@outlook.com');
 
         cy.get('textarea[id="open-text-area"]')
         .should('be.visible')
+        .type('Quero comprar alguns produtos teste delay  teste delay teste delay teste delay teste delay teste delay teste delay', { delay: 10});
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('.success').should('be.visible');
+    });
+
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName')
+        .type('Mari');
+
+        cy.get('#lastName')
+        .type('Gomes');
+
+        cy.get('#email')
+        .type('maregomes.com');
+
+        cy.get('textarea[id="open-text-area"]')
         .type('Quero comprar alguns produtos');
 
         cy.get('button[type="submit"]').click();
 
-        cy.get('span[class="success"]').should('be.visible');
+        cy.get('.error').should('be.visible');
     });
-  })
+
+    it('Se um valor não-numérico for digitado no campo telefone, seu valor continuará vazio', function(){
+        cy.get('#phone')
+        .type('Teste texto')
+        .should('be.empty');
+
+        cy.get('#phone')
+        .type('11923459988')
+        .should('have.value', '11923459988');
+    });
+
+    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#firstName')
+        .type('Mari');
+
+        cy.get('#lastName')
+        .type('Gomes');
+
+        cy.get('#email')
+        .type('maregomes@outlook.com');
+
+        cy.get('textarea[id="open-text-area"]')
+        .type('Quero comprar alguns produtos');
+
+        cy.get('#phone-checkbox').check();
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('.error').should('be.visible');
+
+        cy.get('#phone')
+        .type('11923459988')
+        .should('have.value', '11923459988');
+
+        cy.get('button[type="submit"]').click();
+
+        cy.get('.success').should('be.visible');
+    });
+
+    it('Preenche e limpa os campos', function(){
+        cy.get('#firstName')
+        .type('Mari')
+        .should('have.value', 'Mari')
+        .clear().should('have.value', '');
+
+        cy.get('#lastName')
+        .type('Gomes')
+        .should('have.value', 'Gomes')
+        .clear().should('have.value', '');
+
+        cy.get('#email')
+        .type('maregomes@outlook.com')
+        .should('have.value', 'maregomes@outlook.com')
+        .clear().should('have.value', '');
+    });
+
+    it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.get('button[type="submit"]').click();
+        cy.get('.error').should('be.visible');
+    });
+
+    it('Envia o formulário com sucesso usando um comando customizado', function(){
+        cy.fillMandatoryFieldsAndSubmit()
+        cy.get('.success').should('be.visible');
+    });
+
+    it('Teste de envio de formulário utilizando contains', function(){
+        cy.contains('button', 'Enviar').click();
+        cy.get('.error').should('be.visible');
+    })
+})
   
